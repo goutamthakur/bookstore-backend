86f207b585641683a8c7c31d5eb90cb0795c515a
goutamthakur <goutamthakur2508@gmail.com>
2025-11-02 20:20:12 +0530

    feat(users): added a basic find all users with tcp messages

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  private users: UserDto[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      age: 34,
+    },
+    {
+      id: 2,
+      name: 'Alice Wilderson',
+      age: 26,
+    },
+  ];
+
+  // TODO: Get data from db
+  findAll() {
+    return this.users;
   }
 }
