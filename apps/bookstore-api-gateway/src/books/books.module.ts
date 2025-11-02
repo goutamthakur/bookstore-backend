28d2bafc67dfe9ada697fcf99eae8559184d4497
goutamthakur <goutamthakur2508@gmail.com>
2025-11-03 00:44:51 +0530

    feat(config): api gateway now has useFactory to create booksClient

--- apps/bookstore-api-gateway/src/books/books.module.ts
+++ apps/bookstore-api-gateway/src/books/books.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientConfigModule } from '../client-config/client-config.module';
+import { BOOKS_CLIENT } from './constant';
+import { ClientConfigService } from '../client-config/client-config.service';
+import { ClientProxyFactory } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'BOOKS_CLIENT',
-        transport: Transport.TCP,
-        options: {
-          port: 3002,
-        },
-      },
-    ]),
-  ],
+  imports: [ClientConfigModule],
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [
+    BooksService,
+    {
+      provide: 'BOOKS_CLIENT',
+      useFactory: (configService: ClientConfigService) => {
+        const clientOptions = configService.booksClientOptions;
+        return ClientProxyFactory.create(clientOptions);
+      },
+      inject: [ClientConfigService],
+    },
+  ],
 })
 export class BooksModule {}
--- apps/bookstore-api-gateway/src/bookstore-api-gateway.module.ts
+++ apps/bookstore-api-gateway/src/bookstore-api-gateway.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { ClientConfigModule } from './client-config/client-config.module';
 
 @Module({
-  imports: [UsersModule, BooksModule],
+  imports: [UsersModule, BooksModule, ClientConfigModule],
 })
 export class BookstoreApiGatewayModule {}
