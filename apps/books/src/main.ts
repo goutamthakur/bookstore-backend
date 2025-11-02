ef0413cc6dac820e201f17f7a83382971f6a899f
goutamthakur <goutamthakur2508@gmail.com>
2025-11-02 22:21:37 +0530

    refactor(apps): removed extra files

--- apps/books/src/main.ts
+++ apps/books/src/main.ts
@@ -1,8 +1,23 @@
+/**
+ * Creating a Nest microservice appliction
+ * Setting the BooksAppModule as the root module.
+ * Using Transport type as TCP
+ */
+
 import { NestFactory } from '@nestjs/core';
-import { BooksModule } from './books.module';
+import { BooksAppModule } from './books-app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(BooksModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    BooksAppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3002,
+      },
+    },
+  );
+  await app.listen();
 }
-bootstrap();
+void bootstrap();
